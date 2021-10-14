b98ff59188cba3f535fcd0afde2434bc3535679d
SidraTulMuntha <85743139+SidraTulMuntha@users.noreply.github.com>
2021-10-14 09:35:02 -0700

    python project
    Python program to print positive Numbers in a List

---
+++ python_files/8th_project.py
@@ -0,0 +1,8 @@
+# Python program to print positive Numbers in a List
+
+list1 = [11, -21, 0, 45, 66, -93]
+for num in list1:
+      
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
